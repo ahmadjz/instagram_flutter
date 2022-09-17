039e40537e6c1d3dca949b9d87373503d7e248df
Ahmad jz <ahmadjzz99@gmail.com>
2022-09-17 15:05:34 +0300

    add firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter/responsive/responsive_layout.dart';
 import 'package:instagram_flutter/responsive/web_screen_layout.dart';
 import 'package:instagram_flutter/utlis/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyDCamCBm5AZd29zn_yxlgLNzfI6vz5cPJw",
+          appId: "1:555094935301:web:47978f95eadec874e5d96c",
+          messagingSenderId: "555094935301",
+          projectId: "instagram-clone-96912",
+          storageBucket: "instagram-clone-96912.appspot.com"),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
