c1cf607dcf22370a452103134663678177a76044
Ahmad jz <ahmadjzz99@gmail.com>
2022-10-05 20:33:32 +0300

    update home screen Items

--- lib/utils/global_variable.dart
+++ lib/utils/global_variable.dart
@@ -1,24 +1,19 @@
 // import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/screens/add_post_screen.dart';
 import 'package:instagram_flutter/screens/feed_screen.dart';
-// import 'package:instagram_clone_flutter/screens/add_post_screen.dart';
-// import 'package:instagram_clone_flutter/screens/feed_screen.dart';
-// import 'package:instagram_clone_flutter/screens/profile_screen.dart';
-// import 'package:instagram_clone_flutter/screens/search_screen.dart';
+import 'package:instagram_flutter/screens/profile_screen.dart';
+import 'package:instagram_flutter/screens/search_screen.dart';
 
 const webScreenSize = 600;
 
 List<Widget> homeScreenItems = [
-  // const FeedScreen(),
-  // const SearchScreen(),
-  // const AddPostScreen(),
   const FeedScreen(),
-  const Text('notifications2'),
+  const SearchScreen(),
   const AddPostScreen(),
   const Text('notifications4'),
-  const Text('notifications5'),
-  // ProfileScreen(
-  //   uid: FirebaseAuth.instance.currentUser!.uid,
-  // ),
+  ProfileScreen(
+    uid: FirebaseAuth.instance.currentUser!.uid,
+  ),
 ];
